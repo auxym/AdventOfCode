c88cb48f3838c73a551ab10da80b83f71283925b
Francis Therien <frtherien@gmail.com>
2018-12-01 11:06:35 -0500

    Clean-up and optimization
    Inspired by @narimiran and @jabbalaci, thanks!

--- day1.nim
+++ day1.nim
@@ -1,19 +1,24 @@
-import streams
-import os
 import strutils
+import sequtils
 import sets
+import math
+
+let input = readFile("./day1_input.txt")
+    .splitLines()
+    .filter(proc(x: string): bool = x.len > 0) # parseInt fails on empty last line
+    .map(parseInt)
 
 var 
     sum: int = 0
-    seen = initSet[int]()
+    seen = initSet[int](2^17)
     found_repeat = false
     first = true
 
 while not found_repeat:
-    for line in lines(os.paramStr(1)):
-        sum += parseInt(line)
+    for elem in input:
+        sum += elem
         if not found_repeat and seen.contains(sum):
-            echo "Repeat found_repeat: ", sum
+            echo "Repeat found: ", sum
             found_repeat = true
         seen.incl(sum)
     
