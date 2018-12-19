c6615eadc71bf1a6ef2f3ea2c3a48239a3c89713
Francis Therien <frtherien@gmail.com>
2018-12-18 19:08:29 -0500

    day 14 solution

---
+++ day14.nim
@@ -0,0 +1,49 @@
+import strutils, strformat
+import nimprof
+
+const INPUT = 880751
+
+var recipes = newSeqOfCap[int8](1 shl 25)
+recipes.add 3
+recipes.add 7
+
+var
+    elf1 = 0
+    elf2 = 1
+
+func combine(a, b: int8): seq[int8] =
+    let sum = a + b
+    if sum > 9:
+        result.add(sum div 10)
+    result.add(sum mod 10)
+
+proc show(r: seq[int8], e1, e2: int) =
+    var line = ""
+    for i, elem in r:
+        if i == e1:
+            line.add fmt" ({r[i]:d})"
+        elif i == e2:
+            line.add fmt" [{r[i]:d}]"
+        else:
+            line.add fmt"  {r[i]:d} "
+    echo line
+
+var 
+    part1: string
+    part2: int
+let
+    input_str = $INPUT
+while part1 == "" or part2 == 0:
+    for r in combine(recipes[elf1], recipes[elf2]):
+        recipes.add r
+        if recipes[recipes.len-input_str.len .. recipes.high].join("") == input_str:
+            part2 = recipes.len - input_str.len
+            echo part2
+    
+    elf1 = (elf1 + 1 + recipes[elf1]) mod recipes.len
+    elf2 = (elf2 + 1 + recipes[elf2]) mod recipes.len
+
+    # part 1
+    if (part1 == "") and (recipes.len >= INPUT + 10):
+        part1 = recipes[INPUT..<INPUT+10].join("")
+        echo part1
