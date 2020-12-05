0f30c243feee2c6cc8a977e8270e975da5797a8c
Francis Therien <frtherien@gmail.com>
2020-12-05 11:01:26 -0500

    2020 day 5 part 2

--- 2020/day05.nim
+++ 2020/day05.nim
@@ -1,4 +1,4 @@
-import strutils, sequtils
+import strutils, sequtils, intsets
 
 func getRowOrCol(chars: string): int =
   result = 0
@@ -17,10 +17,29 @@ func getSeatId(text: string): int =
 doAssert getRowOrCol("FBFBBFF") == 44
 doAssert getRowOrCol("RLR") == 5
 
-let passes = readFile("./input/day05_input.txt").strip.splitLines
+let passIds = readFile("./input/day05_input.txt")
+  .strip
+  .splitLines
+  .map(getSeatId)
+  .toIntSet
 
+# Part 1
 var highestId = int.low
-for id in passes.map(getSeatId):
+for id in passIds:
   if id > highestId: highestId = id
 echo highestId
 doAssert highestId == 970
+
+# Part 2
+let maxId = 127 * 8 + 7
+var missingIds = initIntSet()
+for i in countup(0, maxId):
+  if i notin passIds:
+    missingIds.incl i
+
+for id in missingIds:
+  if id == 0 or id == maxId:
+    continue
+  if (id - 1) in missingIds or (id + 1) in missingIds:
+    continue
+  echo id
