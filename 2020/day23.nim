743d71e26f712226d4e6b7632f664cc6c37f4eac
Francis Therien <frtherien@gmail.com>
2020-12-27 20:17:04 -0500

    Performance improvements to day 23

--- 2020/day23.nim
+++ 2020/day23.nim
@@ -1,68 +1,62 @@
-import lists, strutils, strformat
+import strutils, sequtils
 
 const input = "467528193"
 
+# Each index represents a cup value and holds the value of the next cup
+# From a tip on reddit. My previous implementation used a DoublyLinkedRing
+# (from the lists module) combined with a seq[DoublyLinkedNoke] as an index
+# and took about 7 s to solve. This versions runs in about 0.8 s.
+type SeqRing = seq[int]
+
 type GameState = object
-  cups: DoublyLinkedRing[int]
-  cur: DoublyLinkedNode[int]
+  cups: SeqRing
+  cur: int
   minCup: int
   maxCup: int
-  index: seq[DoublyLinkedNode[int]]
 
-func `$`(g: GameState): string =
-  for e in g.cups:
-    if e == g.cur.value:
-      result.add fmt"({e}) "
-    else:
-      result.add $e & " "
-  result = result[0..^2]
+iterator listItems(r: SeqRing, start: int): int =
+  yield start
+  var cur = r[start]
+  while cur != start:
+    yield cur
+    cur = r[cur]
 
 func initGame(text: string): GameState =
+  let first = parseInt($text[0])
+  result.cur = first
   result.minCup = int.high
   result.maxCup = int.low
-  result.cups = initDoublyLinkedRing[int]()
-  result.index = newSeq[DoublyLinkedNode[int]](text.len + 1)
-  for ch in text:
-    let x = ($ch).parseInt
-    if x < result.minCup: result.minCup = x
-    if x > result.maxCup: result.maxCup = x
-    let node = newDoublyLinkedNode(x)
-    result.index[x] = node
-    result.cups.append node
-  result.cur = result.cups.head
 
-proc insertAfter[T](node: var DoublyLinkedNode[T], val: T): DoublyLinkedNode[T] =
-  var newNode = newDoublyLinkedNode[T](val)
-  newNode.prev = node
-  newNode.next = node.next
-  node.next.prev = newNode
-  node.next = newNode
-  return newNode
+  result.cups = newSeq[int](text.len + 1)
+  for (i, chr) in text.pairs:
+    let cur = parseInt($chr)
+    let next = if i == text.high: first else: parseInt($text[i+1])
+    result.cups[cur] = next
+    if cur > result.maxCup: result.maxCup = cur
+    if cur < result.minCup: result.minCup = cur
 
 proc playRound(game: var GameState) =
-  var pickedCups: seq[int]
-  for i in 0..2:
-    pickedCups.add game.cur.next.value
-    game.cups.remove game.cur.next
+  var
+    nextCup = game.cups[game.cur]
+    pickedCups: array[3, int]
+  for i in countDown(2, 0):
+    pickedCups[i] = nextCup
+    nextCup = game.cups[nextCup]
+  game.cups[game.cur] = nextCup
 
-  var destVal = game.cur.value
-  while destVal == game.cur.value or destVal in pickedCups:
+  var destVal = game.cur
+  while destVal == game.cur or destVal in pickedCups:
     destVal.dec
     if destVal < game.minCup: destVal = game.maxCup
 
-  var dest = game.index[destVal]
-  while pickedCups.len > 0:
-    let cupval = pickedCups.pop
-    game.index[cupval] = dest.insertAfter(cupval)
+  for pCup in pickedCups:
+    game.cups[pCup] = game.cups[destVal]
+    game.cups[destVal] = pCup
 
-  game.cur = game.cur.next
+  game.cur = game.cups[game.cur]
 
 func finalScore(g: GameState): string =
-  let one = g.cups.find(1)
-  var cur = one.next
-  while cur != one:
-    result = result & $cur.value
-    cur = cur.next
+  toSeq(g.cups.listItems(1)).join("")[1..^1]
 
 var game = input.initGame
 for i in 0..<100: game.playRound
@@ -74,16 +68,22 @@ doAssert pt1 == "43769582"
 
 func initGamePt2(init: string): GameState =
   result = init.initGame
-  result.index.setLen(1_000_001)
-  for i in (result.maxCup + 1 .. 1_000_000):
-    let node = newDoublyLinkedNode(i)
-    result.cups.append node
-    result.index[i] = node
+  result.cups.setLen(1_000_001)
+
+  let tail = parseInt($init[^1])
+  result.cups[tail] = result.maxCup + 1
+
+  for i in (result.maxCup + 1 ..< 1_000_000):
+    result.cups[i] = i + 1
+  result.cups[1_000_000] = result.cur
   result.maxCup = 1_000_000
 
-func part2Score(g: GameState): int =
-  let one = g.index[1]
-  result = one.next.value * one.next.next.value
+proc part2Score(g: GameState): int =
+  result = 1
+  var cur = 1
+  for i in 0..1:
+    cur = game.cups[cur]
+    result = result * cur
 
 game = input.initGamePt2
 for i in 0..<10_000_000:
