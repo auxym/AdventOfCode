b10e996f8ee2c100ef8dfd60bf8685c5b33fdc18
Francis Therien <frtherien@gmail.com>
2020-12-28 19:31:20 -0500

    2020 day 24 part 2

--- 2020/day24.nim
+++ 2020/day24.nim
@@ -1,7 +1,5 @@
 import tables, sequtils, strutils
 
-type HexAxialCoords = tuple[q, r: int]
-
 type HexDirection = enum
   hdEast = "e"
   hdWest = "w"
@@ -10,7 +8,10 @@ type HexDirection = enum
   hdNorthEast = "ne"
   hdNorthWest = "nw"
 
-type Path = seq[HexDirection]
+type
+  Path = seq[HexDirection]
+  HexAxialCoords = tuple[q, r: int]
+  TileMap = Table[HexAxialCoords, bool]
 
 const Origin: HexAxialCoords = (0, 0)
 
@@ -42,24 +43,63 @@ func tokenizeSteps(text: string): Path =
 assert "nwwswee".tokenizeSteps == @[hdNorthWest, hdWest, hdSouthWest, hdEast, hdEast]
 assert "esew".tokenizeSteps == @[hdEast, hdSouthEast, hdWest]
 
-func moveTo(start: HexAxialCoords, path: Path): HexAxialCoords =
+func moveTo(start: HexAxialCoords, path: openArray[HexDirection]): HexAxialCoords =
   result = start
   for step in path: result = result + step.toVector
 
 assert Origin.moveTo("nwwswee".tokenizeSteps) == Origin
 assert Origin.moveTo("esew".tokenizeSteps) == hdSouthEast.toVector
 
-let inputPaths = readFile("./input/day24_input.txt")
-  .strip.splitLines.map(tokenizeSteps)
+func countBlack(tiles: TileMap): Natural =
+  for t in tiles.values:
+    if t: inc result
 
-func flipTilesAndCount(paths: seq[Path]): Natural =
-  var tileMap: Table[HexAxialCoords, bool]
+func flipTilesAndCount(paths: seq[Path]): (TileMap, Natural) =
+  var tileMap: TileMap
   for pt in paths:
     let
       dest = Origin.moveTo(pt)
       cur = tileMap.getOrDefault(dest, false)
     tileMap[dest] = not cur
-  for t in tileMap.values:
-    if t: inc result
+  result = (tileMap, tileMap.countBlack)
 
-echo inputPaths.flipTilesAndCount
+
+let inputPaths = readFile("./input/day24_input.txt")
+  .strip.splitLines.map(tokenizeSteps)
+
+let (tileMap, pt1Count) = inputPaths.flipTilesAndCount
+echo pt1Count
+doAssert pt1Count == 341
+
+# Part 2
+iterator neighbors(c: HexAxialCoords): HexAxialCoords =
+  for d in HexDirection:
+    yield c.moveTo [d]
+
+func evolve(tiles: TileMap): TileMap =
+  var stack = toSeq(tiles.keys)
+
+  while stack.len > 0:
+    let
+      t = stack.pop()
+      tileState = tiles.getOrDefault(t, false)
+    var nbcountBlack = 0
+    for nb in t.neighbors:
+      if tiles.getOrDefault(nb, false):
+        inc nbcountBlack
+      if tileState and nb notin tiles:
+        stack.add nb
+
+    if tileState and (nbCountBlack == 0 or nbCountBlack > 2):
+      result[t] = false
+    elif not tileState and nbcountBlack == 2:
+      result[t] = true
+    else:
+      result[t] = tileState
+
+var artExhibit = tileMap
+for i in 1..100:
+  artExhibit = artExhibit.evolve
+let pt2 = artExhibit.countBlack
+echo pt2
+doAssert pt2 == 3700
