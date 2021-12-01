f8f51910188fc80ed724c76f7b4c2903ed60c09a
Francis Thérien <francis.therien@usherbrooke.ca>
2021-12-01 09:11:19 -0500

    Day 1 part 2

--- 2021/day01.nim
+++ 2021/day01.nim
@@ -9,3 +9,26 @@ for i in 1..input.high:
 
 doAssert incCount == 1752
 echo incCount
+
+func windowSum(elems: seq[int], at: Natural, wsize: Natural): int = 
+  for j in at ..< (at + wsize):
+    result.inc elems[j]
+
+func countWindowed(elems: seq[int], wsize: Natural): int =
+  var
+    curWindow: int
+    prevWindow: int
+
+  for i in 0 .. (elems.len - wsize):
+    curWindow = windowSum(elems, i, wsize)
+
+    if i == 0:
+      continue
+
+    if curWindow > prevWindow:
+      result.inc
+    prevWindow = curWindow
+
+let pt2 = countWindowed(input, 3)
+echo pt2
+doAssert pt2 == 1781
