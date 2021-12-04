c53a7d457f0523f3c4d57d338abb82831aee8ee7
Francis Therien <frtherien@gmail.com>
2021-12-04 09:08:27 -0500

    Day 4 part 1

---
+++ 2021/day04.nim
@@ -0,0 +1,69 @@
+import std/strutils
+import std/sequtils
+import utils
+
+type
+  BingoCard = seq[seq[int]]
+  BingoCardState = seq[seq[bool]]
+
+const CardSize = 5
+
+func parseInput(s: string): (seq[int], seq[BingoCard]) =
+  let chunks = s.strip().split("\n\n")
+  result[0] = chunks[0].getInts
+
+  for i in 1 .. chunks.high:
+    let card: BingoCard = chunks[i].strip().splitLines().map(getInts)
+    assert card.len == CardSize
+    for row in card: assert row.len == CardSize
+    result[1].add card
+
+let (bingoNumbers, bingoCards) = readFile("./input/day04_input.txt").parseInput
+
+iterator rows[T](s: seq[seq[T]]): seq[T] =
+  for row in s:
+    yield row
+
+iterator columns[T](s: seq[seq[T]]): seq[T] =
+  for i in 0 ..< CardSize:
+    yield s.mapIt(it[i])
+
+proc update(state: var BingoCardState, card: BingoCard, n: int) =
+  for i in 0 ..< CardSize:
+    for j in 0 ..< CardSize:
+      if card[i][j] == n:
+        state[i][j] = true
+
+proc checkWin(state: BingoCardState): bool =
+  for rw in state.rows:
+    if rw.allIt(it):
+      return true
+
+  for col in state.columns:
+    if col.allIt(it):
+      return true
+
+  return false
+
+func score(card: BingoCard, state: BingoCardState, n: int): int =
+  for i in 0 ..< CardSize:
+    for j in 0 ..< CardSize:
+      if not state[i][j]: result.inc card[i][j]
+
+  result = result * n
+
+func initState(): BingoCardState =
+  newSeqWith(CardSize, newSeqWith(CardSize, false))
+
+func playGame(numbers: seq[int], cards: seq[BingoCard]): int =
+  var gameState: seq[BingoCardState] = newSeqWith(cards.len, initState())
+
+  for n in numbers:
+    for i in 0 .. cards.high:
+      gameState[i].update(cards[i], n)
+      if gameState[i].checkWin:
+        return cards[i].score(gameState[i], n)
+
+let pt1Score = playGame(bingoNumbers, bingoCards)
+echo pt1Score
+doAssert pt1Score == 63424
