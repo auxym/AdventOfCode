be6970ab4324283f30ff1403f553b156cd68f4a4
Francis Therien <frtherien@gmail.com>
2021-12-09 21:22:11 -0500

    2021 day 9 part 2

--- 2021/day09.nim
+++ 2021/day09.nim
@@ -1,6 +1,7 @@
-
+import std/tables
 import std/sequtils
 import std/strutils
+import std/algorithm
 import utils
 
 #let input = """
@@ -14,6 +15,10 @@ let input = readFile("./input/day09_input.txt")
 
 type SeaFloorMap = seq[seq[int]]
 
+func parseInput(s: string): SeaFloorMap =
+  for line in s.strip.splitLines:
+    result.add line.mapIt(parseInt($it))
+
 func `[]`(g: SeaFloorMap, v: Vector): int =
   g[v.y][v.x]
 
@@ -22,25 +27,21 @@ iterator tilePairs(g: SeaFloorMap): (Vector, int) =
     for (j, elem) in row.pairs:
       yield ((j, i), elem)
 
-iterator adjacent(g: SeaFloorMap, at: Vector): int =
+iterator adjacent(g: SeaFloorMap, at: Vector): Vector =
   if at.x > 0:
-    yield g[at.y][at.x - 1]
+    yield (at.x - 1, at.y)
   if at.x < g[at.y].high:
-    yield g[at.y][at.x + 1]
+    yield (at.x + 1, at.y)
   if at.y > 0:
-    yield g[at.y - 1][at.x]
+    yield (at.x, at.y - 1)
   if at.y < g.high:
-    yield g[at.y + 1][at.x]
+    yield (at.x, at.y + 1)
 
 func isLowPoint(g: SeaFloorMap, at: Vector): bool =
-  for adjHeight in g.adjacent(at):
-    if adjHeight <= g[at]:
+  for pt in g.adjacent(at):
+    if g[pt] <= g[at]:
       return false
   return true
-
-func parseInput(s: string): SeaFloorMap =
-  for line in s.strip.splitLines:
-    result.add line.mapIt(parseInt($it))
 
 func sumRiskLevels(g: SeaFloorMap): int =
   for (v, height) in g.tilePairs:
@@ -53,4 +54,29 @@ let
 
 echo pt1
 
+# Part 2
 
+func findBasin(g: SeaFloorMap, at: Vector): Vector =
+  result = at
+  while not g.isLowPoint(result):
+    var
+      next: Vector
+      minAdjHeight: int = int.high
+    for neighbor in g.adjacent(result):
+      if g[neighbor] < g[result] and g[neighbor] < minAdjHeight:
+        next = neighbor
+        minAdjHeight = g[neighbor]
+    assert minAdjHeight < int.high
+    result = next
+
+func CountBasinSizes(g: SeaFloorMap): CountTable[Vector] =
+  for (v, height) in g.tilePairs:
+    if height == 9:
+      continue
+    result.inc g.findBasin(v)
+
+let
+  basinSizes = fmap.CountBasinSizes
+  pt2 = toSeq(basinSizes.values).sorted[^3 .. ^1].foldl(a * b)
+
+echo pt2
