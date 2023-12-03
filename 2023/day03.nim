608008f3971c4ce55c7539922c8b6240d8d3d9af
Francis Therien <frtherien@gmail.com>
2023-12-03 17:11:33 -0500

    2023 day 3 part 2

--- 2023/day03.nim
+++ 2023/day03.nim
@@ -37,3 +37,39 @@ let pt1 = input.findPartNumbers.sum
 echo pt1
 
 
+# Part 2
+
+type Coords = tuple[a, b: int]
+
+func maxNorm(u, v: Coords): Natural = max(abs(u.a - v.a), abs(u.b - v.b))
+
+func isGear(input: seq[string], loc: Coords): (bool, Natural) =
+  if input[loc.a][loc.b] != '*':
+    return
+
+  var pnums: seq[int]
+
+  var surLines = @[loc.a]
+  if loc.a > 0: surLines.add loc.a - 1
+  if loc.a < input.high: surLines.add loc.a + 1
+
+  for lineNum in surLines:
+    for match in input[lineNum].findAll(re2"\d+"):
+      if maxNorm(loc, (lineNum, match.boundaries.a)) == 1 or
+      maxNorm(loc, (lineNum, match.boundaries.b)) == 1:
+        pnums.add input[lineNum][match.boundaries].parseInt
+
+  if pnums.len == 2:
+    result[0] = true
+    result[1] = pnums[0] * pnums[1]
+
+func findGears(input: seq[string]): seq[Natural] =
+  let colHigh = input[0].high
+  for lineNum in input.low .. input.high:
+    for colNum in 0 .. colHigh:
+      let (thisIsGear, ratio) = isGear(input, (lineNum, colNum))
+      if thisIsGear:
+        result.add ratio
+
+let pt2 = input.findGears.sum
+echo pt2
