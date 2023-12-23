b8f8f76f54bc0be25aa2962842589b96d9c67a56
Francis Therien <frtherien@gmail.com>
2023-12-23 17:14:51 -0500

    2023 day 16 part 2

--- 2023/day16.nim
+++ 2023/day16.nim
@@ -48,13 +48,12 @@ iterator splitV(b: Beam): Beam =
     yield b.cw.next
     yield b.ccw.next
 
-func energize(cont: Contraption): HashSet[Vector] =
+func energize(cont: Contraption; start: Beam): HashSet[Vector] =
   var
     stack: seq[Beam]
     seen: HashSet[Beam]
 
-  stack.add Beam(loc: (0, 0), dir: (1, 0))
-
+  stack.add start
   while stack.len > 0:
     let beam = stack.pop
 
@@ -89,18 +88,46 @@ func energize(cont: Contraption): HashSet[Vector] =
     else:
       assert false
 
-proc showEnergized(cont: Contraption, enset: HashSet[Vector]) =
-  for irow in 0 .. cont.nrows:
-    for icol in 0 .. cont.ncolumns:
+proc showEnergized(cont: Contraption; enset: HashSet[Vector]) =
+  for irow in 0..cont.nrows:
+    for icol in 0..cont.ncolumns:
       if (icol, irow) in enset:
         stdout.write '#'
       else:
         stdout.write '.'
     stdout.write '\n'
 
-let energized = input.energize
-input.showEnergized(energized)
+let energized = input.energize(start = Beam(loc: (0, 0), dir: (1, 0)))
+#input.showEnergized(energized)
 echo energized.card
 
 #let pt1 = input.energize.card
 #echo pt1
+
+# Part 2
+
+iterator allStartingConfigs(cont: Contraption): Beam =
+  let
+    yhigh = cont.nrows - 1
+    xhigh = cont.ncolumns - 1
+
+  # Top and bottom edged
+  for x in 0..<cont.ncolumns:
+    yield Beam(loc: (x, 0), dir: (0, 1))
+    yield Beam(loc: (x, yhigh), dir: (0, -1))
+
+  # Left and right edges
+  for y in 0..<cont.nrows:
+    yield Beam(loc: (0, y), dir: (1, 0))
+    yield Beam(loc: (xhigh, y), dir: (-1, 0))
+
+let pt2 =
+  block:
+    var m = 0
+    for cfg in input.allStartingConfigs:
+      let enerCount = input.energize(cfg).card
+      if enerCount > m:
+        m = enerCount
+    m
+
+echo pt2
