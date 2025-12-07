f6e97430f17593bde7473706ee7656f437b8a0b8
Francis Thérien <frtherien@gmail.com>
2025-12-07 18:31:07 -0500

    Day 1 part 2

--- 2025-go/cmd/day01/day01.go
+++ 2025-go/cmd/day01/day01.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"strings"
+
 	"github.com/auxym/AdventOfCode/2025-go/utils"
 )
 
@@ -20,10 +22,40 @@ func applyInstruction(current int, instruction string) int {
 		num = -num
 	}
 	new := (current + num) % 100
+
 	if new < 0 {
 		new += 100
 	}
+
 	return new
+}
+
+func applyInstruction2(current int, instruction string) (int, int) {
+	var dir rune
+	var num int
+	_, err := fmt.Sscanf(strings.TrimSpace(instruction), "%c%d", &dir, &num)
+	if err != nil {
+		panic(err)
+	}
+
+	if dir == 'L' {
+		num = -num
+	}
+
+	new := current + num
+	passedZero := utils.Abs(new / 100)
+	new = new % 100
+
+	if new < 0 {
+		new += 100
+		if current > 0 {
+			passedZero++
+		}
+	} else if (new == 0) && (dir == 'L') {
+		passedZero++
+	}
+
+	return new, passedZero
 }
 
 func part1() int {
@@ -40,11 +72,19 @@ func part1() int {
 }
 
 func part2() int {
-	utils.LoadInput(day)
-	return 0
+	instructions := utils.Lines(utils.LoadInput(day))
+	current := 50
+	password := 0
+
+	var passedZero int
+	for _, instr := range instructions {
+		current, passedZero = applyInstruction2(current, instr)
+		password += passedZero
+	}
+	return password
 }
 
 func main() {
 	utils.ShowAnswer(1, part1(), 1195, true)
-	utils.ShowAnswer(2, part2(), 0, false)
+	utils.ShowAnswer(2, part2(), 6770, true)
 }
--- 2025-go/utils/utils.go
+++ 2025-go/utils/utils.go
@@ -29,3 +29,10 @@ func ShowAnswer[T comparable](part int, x, expected T, doCheck bool) {
 		fmt.Printf("Part %d: %v\n", part, x)
 	}
 }
+
+func Abs[T ~int | ~int8 | ~int16 | ~int32 | ~int64](x T) T {
+    if x < 0 {
+        return -x
+    }
+    return x
+}
